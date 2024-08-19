f2249091e5870de40107faa324c176b1f5ab5767
Lucas Calabresi <lucas.calabresi@hotmail.com>
2024-08-19 19:34:40 -0300

    Mundanças no arquivo de configuração pronta

--- config.py
+++ config.py
@@ -1,16 +1,26 @@
 #Configuração para conectar com postgre
+#E também declarar as funções aqui
 import psycopg2
 
 coneccao = psycopg2.connect(
-    database = '',
-    user = '',
-    password = '',
-    host = '',
-    port = ''
+    database = 'country_finder',
+    user = 'postgres',
+    password = '99!21?80',
+    host = 'localhost',
+    port = '5432'
 )
 
 #Executa meus comandos dentro dentro do postgre
 curs = coneccao.cursor()
+#Vai conter todos os registros retornado por 'curs'
+resultados = curs.fetchall()
+
 
 #Confirma todas as alterações feitas na transação atual do banco de dados
-coneccao.commit()
+commit = coneccao.commit()
+
+
+#Fechar minha conecção com o banco de dados (Por uma questão de segurança)
+fecha_conec = coneccao.close()
+#Fecha o meu cursor
+fehca_cur = curs.close()
