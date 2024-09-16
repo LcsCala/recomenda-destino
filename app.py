b5da01d54d374c1bfefd7490cb1c603563ee8bd8
Lucas Calabresi <lucas.calabresi@hotmail.com>
2024-09-16 10:14:04 -0300

    Adicionando .env e .gitignore pra modificar os arquivos que continham as informações privadas

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import config
+import bd
 
 app = Flask(__name__)
 
@@ -7,6 +7,28 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Bem vindo"
+
+#Pagina que mostra a tabela
+@app.route('/consulta', methods = ['GET'])
+def tabela ():
+    resultados = bd.mostra_tabela()
+    colunas = [
+        'coluna1', 
+        'coluna2',
+        'coluna3',
+        'coluna4',
+        'coluna5',
+        'coluna6',
+        'coluna7',
+        'coluna8',
+        'coluna9',
+        'coluna10',
+        'coluna11',
+        'coluna12',
+        'coluna13',
+        ]
+    resultados_formatados = [dict(zip(colunas, linha)) for linha in resultados]
+    return jsonify(resultados_formatados)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- bd.py
+++ bd.py
@@ -1,12 +1,19 @@
 #Configuração para conectar com postgre
 #E também declarar as variaveis aqui
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Carrega as variáveis do .env que vai conter as informações que não desejoj que apareça
+
+bd_senha = os.getenv("bd_senha")
+
 
 def conectar():
     return psycopg2.connect(
         database = 'country_finder',
         user = 'postgres',
-        password = '99!21?80',
+        password = bd_senha,
         host = 'localhost',
         port = '5432'
     )
